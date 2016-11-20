10af50430d96b51da19b86c0e5cf976e046e6bfb
Pentai <penthaizza@gmail.com>
2016-11-20 16:36:17 +0700

    แก้บัค Register

--- src/java/model/Register.java
+++ src/java/model/Register.java
@@ -22,15 +22,57 @@ public class Register {
     private String email;
     private String address;
 
-    public Register(String username, String password, String fname, String lname, String email, String address, String city, String country) {
+    public Register() {
+    }
+
+    public Register(String username, String password, String fname, String lname, String email, String address) {
         this.username = username;
         this.password = password;
         this.fname = fname;
         this.lname = lname;
         this.email = email;
         this.address = address;
-        this.city = city;
-        this.country = country;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getAddress() {
@@ -40,91 +82,16 @@ public class Register {
     public void setAddress(String address) {
         this.address = address;
     }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-    private String city;
-    private String country;
-
-    public Register() {
-    }
     
-
-    public void setUsername(String username){
-      this.username = username;
-    }
-    
-    public String getUsername(){
-      return this.username;
-    }
-    
-    public void setPassword(String password){
-      this.password = password;
-    }
-    
-    public String getPassword(){
-      return this.password;
-      
-      
-    }
-
- 
-
-
-
-  public String getFname() {
-    return fname;
-  }
-
-  public void setFname(String fname) {
-    this.fname = fname;
-  }
-
-  public String getLname() {
-    return lname;
-  }
-
-  public void setLname(String lname) {
-    this.lname = lname;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
     @Override
     public String toString() {
-        return "Register{" + "username=" + username + ", password=" + password + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", address=" + address + ", city=" + city + ", country=" + country + '}';
+        return "Register{" + "username=" + username + ", password=" + password + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", address=" + address;
     }
-
-
-
-    
-
- 
 
     public static void signUp(String username, String password, String fname, String lname, String email, String address) throws ClassNotFoundException, SQLException {
         Connection con = ConnectionBuilder.getCon();
         PreparedStatement pst = con.prepareStatement("insert into accounts (username,password,fname,lname,email,address) values (?,?,?,?,?,?)");
-        
-        
+
         pst.setString(1, username);
         pst.setString(2, password);
         pst.setString(3, fname);
@@ -133,9 +100,7 @@ public class Register {
         pst.setString(6, address);
         pst.executeUpdate();
         con.close();
-        
+
     }
-    
-  }
 
-
+}
